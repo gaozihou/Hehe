6d7e16791078e48e93a480da3f2755af2c3ca708
paul <gaozihou@2008.sina.com>
2015-05-11 04:47:49 +0800

    Gao's first

--- app/src/main/java/com/example/paul/hehe/MainActivity.java
+++ app/src/main/java/com/example/paul/hehe/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private int a = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
